8ee1fad6cb248d39890c3d2bd624dd8119ccdf1c
princekamariya <kamriyaprince@gmail.com>
2025-10-19 11:23:40 +0530

    added basic files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import "./App.css";
+
+function App() {
+    return (
+        <div className="text-3xl font-bold text-cyan-300">
+            <h3>Hello Lets Build</h3>
+        </div>
+    );
+}
+
+export default App;
